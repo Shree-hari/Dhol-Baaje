f847b67c83d6ec61ef313eab37c76aa170106766
Shree-hari <harshmacc@gmail.com>
2019-11-12 16:35:54 +0530

    ADDED ANIMATION

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ for(var i=0; i < numberOfDrumButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
-
+    buttonAnimation(buttonInnerHTML);
   });
 
   document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
   });
   function makeSound(key){
     switch (key) {
@@ -43,5 +44,13 @@ for(var i=0; i < numberOfDrumButtons; i++){
       default:
 
     }
+  }
+  function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+ currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+      activeButton.classList.remove("pressed");
+    }, 100 );
   }
 }
